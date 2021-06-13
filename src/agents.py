98a0a4221cda61773fd627df5c77f003cf337e47
John Andronis <iandronis@omilia.com>
2021-06-14 02:35:38 +0300

    improv: agents and util functions
    * adjust agents:
      - correct 'agent_random_prefer_winning_move'
      - add 'agent_random_prefer_winning_avoid_losing_move'
    
    * improve/correct util functions:
      - 'play_piece'
      - '_check_board'
      - 'check_winning_move'
      - 'check_losing_move'

--- src/agents.py
+++ src/agents.py
@@ -1,6 +1,12 @@
 import random
 
-from src.utils import convert_board_1d_to_2d, play_piece
+from src.utils import (
+    check_losing_move,
+    check_winning_move,
+    convert_board_1d_to_2d,
+)
+
+BOARD_MARKS = [1, 2]
 
 
 def agent_random(obs, config):
@@ -17,11 +23,38 @@ def agent_leftmost(obs, config):
     return valid_moves[0]
 
 
-def agent_random_check_winning_move(obs, config):
+def agent_random_prefer_winning_move(obs, config):
     board = convert_board_1d_to_2d(obs.board, config)
     agent_piece = obs.mark
-    valid_moves = [col for col in range(config.columns) if obs.board[col] == 0]
+    valid_moves = [col for col in range(config.columns) if board[0][col] == 0]
+
     for move in valid_moves:
-        if play_piece(board, move, agent_piece, config):
+        if check_winning_move(board, config, move, agent_piece):
             return move
+
+    return random.choice(valid_moves)
+
+
+def agent_random_prefer_winning_avoid_losing_move(obs, config):
+    board = convert_board_1d_to_2d(obs.board, config)
+    agent_piece = obs.mark
+
+    valid_moves = [col for col in range(config.columns) if board[0][col] == 0]
+    losing_moves = []
+    for move in valid_moves:
+        if check_winning_move(board, config, move, agent_piece):
+            return move
+
+        opponent_piece = [mark for mark in BOARD_MARKS if mark != agent_piece][
+            0
+        ]
+        if check_losing_move(board, config, move, agent_piece, opponent_piece):
+            losing_moves.append(move)
+
+    valid_remove_losing_moves = [
+        move for move in valid_moves if move not in losing_moves
+    ]
+    # there are cases where you lose with any move you take
+    if valid_remove_losing_moves:
+        valid_moves = valid_remove_losing_moves
     return random.choice(valid_moves)
--- src/utils.py
+++ src/utils.py
@@ -15,13 +15,14 @@ def convert_board_1d_to_2d(board: list[int], config) -> np.ndarray:
     return np.asarray(board).reshape(config.rows, config.columns)
 
 
-def play_piece(board: np.ndarray, col: int, piece: int, config):
+def play_piece(board: np.ndarray, config, col: int, piece: int) -> np.ndarray:
     invalid_move = True
 
     next_board = board.copy()
     for row in range(config.rows - 1, -1, -1):
         if next_board[row][col] == 0:
             invalid_move = False
+            break
 
     if invalid_move:
         raise InvalidArgument("Invalid move detected.")
@@ -30,26 +31,24 @@ def play_piece(board: np.ndarray, col: int, piece: int, config):
     return next_board
 
 
-def check_winning_move(obs, config, col, piece):
-    board = convert_board_1d_to_2d(board=obs.board, config=config)
-    next_board = play_piece(board, col, piece, config)
+def _check_board(board: np.ndarray, config, piece: int) -> bool:
     # horizontal
     for row in range(config.rows):
         for col in range(config.columns - (config.inarow - 1)):
-            window = list(next_board[row, col : col + config.inarow])
+            window = list(board[row, col : col + config.inarow])
             if window.count(piece) == config.inarow:
                 return True
     # vertical
     for row in range(config.rows - (config.inarow - 1)):
         for col in range(config.columns):
-            window = list(next_board[row : row + config.inarow, col])
+            window = list(board[row : row + config.inarow, col])
             if window.count(piece) == config.inarow:
                 return True
     # positive diagonal
     for row in range(config.rows - (config.inarow - 1)):
         for col in range(config.columns - (config.inarow - 1)):
             window = list(
-                next_board[
+                board[
                     range(row, row + config.inarow),
                     range(col, col + config.inarow),
                 ]
@@ -60,11 +59,31 @@ def check_winning_move(obs, config, col, piece):
     for row in range(config.inarow - 1, config.rows):
         for col in range(config.columns - (config.inarow - 1)):
             window = list(
-                next_board[
+                board[
                     range(row, row - config.inarow, -1),
                     range(col, col + config.inarow),
                 ]
             )
             if window.count(piece) == config.inarow:
                 return True
+    return False
+
+
+def check_winning_move(board: np.ndarray, config, col: int, piece: int) -> bool:
+    next_board = play_piece(board, config, col, piece)
+    return _check_board(next_board, config, piece)
+
+
+def check_losing_move(
+    board: np.ndarray, config, col: int, piece: int, opponent_piece: int
+) -> bool:
+    next_board = play_piece(board, config, col, piece)
+    opponent_valid_moves = [
+        col for col in range(config.columns) if next_board[0][col] == 0
+    ]
+    for opponent_move in opponent_valid_moves:
+        if check_winning_move(
+            next_board, config, opponent_move, opponent_piece
+        ):
+            return True
     return False
